e088f968eb1f5656ee598bfeb5cb3d4b79ec51d8
NazmulHayat <nazmulhayat588@gmail.com>
2021-07-02 18:43:45 +0600

    fk this shit

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -6,7 +6,9 @@ export default {
     data (){
         return {
             slides: [
-                {idx:'1', img: require('@/assets/networking.png'), header: "Idea Sprint", text:""}, //idea contest
+                {idx:'1', img: require('@/assets/networking.png'), header: "Idea Sprint",
+                  text: ""
+                }, //idea contest
                 {idx:'2', img: require('@/assets/frog.png'), header: "Mathletics", text:""}, //Mathletics 
                 {idx:'3', img: require('@/assets/plain.png'), header: "CodeJam", text:""}, //Programming Contest
                 {idx:'4', img: require('@/assets/dewal.jpeg'), header: "Easter Egg", text:""}, //Easter Egg
@@ -24,17 +26,31 @@ export default {
         :key="i"
         :src="item.img"
       >
-      <span id="header" class="white--text m">{{item.header}}</span>
+      <div class="BOX">
+        <div id="header" class="white--text">{{item.header}}</div>
+        <div id="content" class="white--text">{{item.text}}</div>
+      </div>
       </v-carousel-item>
     </v-carousel>
 </template>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@600&display=swap');
+  .container{
+    width: 200px;
+  }
   #header{
     font-family: 'Chakra Petch', sans-serif;
-    font-size: 0.75em;
-    padding: 15px;
+    font-size: 0.6em;
+    padding-left: 20px;
+  }
+  #content{
+    font-size: 0.25em;
+    padding-right: 20px;
+    padding-left: 20px; 
+    white-space: pre;
+    word-wrap: break-word;
+    display: inline-block;
   }
 </style>
 
---
+++ src/components/Timer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="flipdown" class="flipdown"></div>
+</template>
+
+<script>
+
+import flipdown from '../assets/flipdown.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+
+        console.log("asdfasdfasdf", document.getElementById('flipdown'));
+
+var fku = new flipdown(twoDaysFromNow, document.getElementById("flipdown"), {
+    theme: 'dark',
+    headings: ["Days", "Hours", "Minutes", "Seconds"]
+});
+fku.start();
+    console.log("adf");
+})
+export default {
+    data () {
+        return {}
+    }
+}
+
+</script>
+
+<style>
+    @import '../assets/flipdown.css';
+</style>
