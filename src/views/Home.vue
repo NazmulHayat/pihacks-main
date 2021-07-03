a5436b8566ae1f2803e66f9c04d611de5e49e3ea
FieryRMS <raiyanmohdshahrms@gmail.com>
2021-07-03 17:19:40 +0600

    fixed merging issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,30 +4,28 @@
     <FAQs/>
     <Timer/>
     <div class="main-text pa-16">
-        <div class="main-head text-h4">About the Event</div>
+        <div class="main-head text-h4">About PIHACKS</div>
         <div class="white--text text-h6">
             <p class="p1">
             Tired of confiding your ideas in your grim pc screens? Presidency International School is proud to unveil “PiHacks 2021”, the first-ever high school hack fest in the nation. Technology, combining logical analysis, science and creativity in one facile format, has become a powerful medium for real change. Since its birth, Bangladesh has accomplished numerous technological feats, and at the cornerstone of all technological advancements inhabits “Digital Bangladesh”. We believe it is of utmost importance that we utilize our increasing dependence on technology so that we can work towards improving the lives of people. We aim to bring you an amalgamation of fun, learning and competitiveness where you will be able to engage in intellectual warfare and stimulate your passion with your fellow tech enthusiasts. </p>
         </div>
     </div>  
+    <TypeWriter/>
   </div>
 </template>
 
 <script>
-
 import Carousel from '@/components/Carousel.vue';
 import Timer from '@/components/Timer.vue';
-
-
+import TypeWriter from '@/components/TypeWriter.vue';
+console.log(Timer);
 export default {
-  components: {Carousel, Timer},
+  components: {Carousel, Timer, TypeWriter},
   data: () => ({
     
   }),
 };
 </script>
-
-
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@500&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap');
